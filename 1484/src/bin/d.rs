13d739e491733b44bdeaf7f5764dd159e8577a77
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-21 16:49:20 +0000

    1484

---
+++ 1484/src/bin/d.rs
@@ -0,0 +1,79 @@
+#[rustfmt::skip]
+mod fast_input {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner {buf:Vec<u8>,pos: usize,}
+    impl Scanner {
+        pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
+            let mut buf = Vec::with_capacity(estimated);let _=std::io::copy(&mut reader,&mut buf).unwrap();if buf.last()!=Some(&b'\n'){panic!("{}", 0);}
+            Scanner { buf, pos: 0 }
+        }
+        #[inline]
+        pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{
+            let mut start=None;loop{match(self.buf[self.pos],start.is_some()){(b' ',true)|(b'\n', true)|(b'\r', true)=>break,(_, true)|(b' ', false)|(b'\n',false)|(b'\r', false)=>self.pos+=1,(_, false)=>start=Some(self.pos),}}let target=&self.buf[start.unwrap()..self.pos];
+            unsafe { std::str::from_utf8_unchecked(target) }.parse().unwrap()
+        }
+    }
+}
+fn main() {
+    let mut sc = fast_input::Scanner::new(std::io::stdin().lock(), 4096);
+
+    input!(sc = sc, t: usize);
+    for _ in 0..t {
+        input!(sc = sc, n: usize, a: [i64; n]);
+        let mut ans = vec![];
+        let mut is_ok = vec![true; n];
+        let mut buff = -1;
+        let mut change_count = 0;
+        for _ in 0..n + 5 {
+            let mut is_changed = false;
+            for j in 0..n {
+                if !is_ok[j] {
+                    continue;
+                }
+                if buff == -1 {
+                    buff = a[j];
+                } else if gcd(buff, a[j]) == 1 {
+                    is_changed = true;
+                    is_ok[j] = false;
+                    buff = -1;
+                    ans.push(j + 1);
+                } else {
+                    buff = a[j];
+                }
+            }
+            if !is_changed {
+                change_count += 1;
+                if change_count > 1 {
+                    break;
+                }
+            }
+        }
+
+        if ans.is_empty() {
+            println!("{}", ans.len());
+            continue;
+        } else {
+            print!("{} ", ans.len());
+        }
+
+        for (i, &v) in ans.iter().enumerate() {
+            if i == ans.len() - 1 {
+                println!("{}", v);
+            } else {
+                print!("{} ", v);
+            }
+        }
+    }
+}
+pub fn gcd(m: i64, n: i64) -> i64 {
+    if m == 0 {
+        n.abs()
+    } else {
+        gcd(n % m, m)
+    }
+}
+pub fn lcm(m: i64, n: i64) -> i64 {
+    m * n / gcd(m, n)
+}
