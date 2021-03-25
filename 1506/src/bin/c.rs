b5a72af07cdd377204985d3db2833c4029f8cee2
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-25 17:05:05 +0000

    1506

---
+++ 1506/src/bin/c.rs
@@ -0,0 +1,64 @@
+#[rustfmt::skip]
+mod fast_input {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner {buf:Vec<u8>,pos: usize,}
+    impl Scanner {
+        pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
+            let mut buf = Vec::with_capacity(estimated);let _=std::io::copy(&mut reader,&mut buf).unwrap();if buf.last()!=Some(&b'\n'){panic!("{}", 0);}
+            Scanner { buf, pos: 0 }
+        }
+        #[inline]
+        pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{
+            let mut start=None;loop{match(self.buf[self.pos],start.is_some()){(b' ',true)|(b'\n', true)|(b'\r', true)=>break,(_, true)|(b' ', false)|(b'\n',false)|(b'\r', false)=>self.pos+=1,(_, false)=>start=Some(self.pos),}}let target=&self.buf[start.unwrap()..self.pos];
+            unsafe { std::str::from_utf8_unchecked(target) }.parse().unwrap()
+        }
+    }
+}
+fn main() {
+    let mut sc = fast_input::Scanner::new(std::io::stdin().lock(), 4096);
+
+    input!(sc = sc, t: usize);
+
+    for _ in 0..t {
+        input!(sc = sc, s: Chars, t: Chars);
+        let mut max = 0;
+        let len = s.len().min(t.len());
+        let (s, t) = if s.len() > t.len() { (t, s) } else { (s, t) };
+        for i in (1..len + 1).rev() {
+            for j in 0..len - i + 1 {
+                if rolling_hash(&s, &t, j, i) {
+                    max = max.max(i);
+                }
+            }
+        }
+        println!("{}", (s.len() + t.len() - max * 2));
+    }
+}
+
+pub fn rolling_hash(s: &[char], t: &[char], start: usize, l: usize) -> bool {
+    let b: u128 = 2u128.pow(61) - 1;
+
+    let pow_b = b.wrapping_pow(l as u32);
+
+    let mut target_hash: u128 = 0;
+    let mut base_hash: u128 = 0;
+    for k in 0..l {
+        base_hash = base_hash.wrapping_mul(b) + s[start + k] as u128;
+        target_hash = target_hash.wrapping_mul(b) + t[k] as u128;
+    }
+
+    for k in 0..t.len() - l + 1 {
+        if target_hash == base_hash {
+            return true;
+        }
+        if l + k < t.len() {
+            target_hash = target_hash
+                .wrapping_mul(b)
+                .wrapping_add(t[l + k] as u128)
+                .wrapping_sub((t[k] as u128).wrapping_mul(pow_b));
+        }
+    }
+    false
+}
