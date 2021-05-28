0c757d0eb0f5b969e898e0465902578e4b4d4bf6
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-05-28 19:25:33 +0000

    1526

---
+++ 1526/src/bin/a.rs
@@ -0,0 +1,47 @@
+use std::mem::swap;
+
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+fn main() {
+    let mut sc = io_pro::Scanner::new(std::io::stdin().lock());
+    input!(sc = sc, t: usize);
+    for _ in 0..t {
+        input!(sc = sc, n: usize, a: [i64; 2 * n]);
+        let mut a = a;
+        let len = a.len();
+        a.sort_unstable();
+
+        for i in 0..n * 2 - 2 {
+            let mut is_none = true;
+            for j in 0..n * 2 - 2 {
+                if a[j] + a[(j + 2) % a.len()] == a[(j + 1) % a.len()] * 2 {
+                    is_none = false;
+                    let p1: *mut i64 = &mut a[(j + 2) % len];
+                    let p2: *mut i64 = &mut a[(j + 1) % len];
+                    unsafe {
+                        p1.swap(p2);
+                    }
+                }
+            }
+            if is_none {
+                break;
+            }
+        }
+        for i in 0..n * 2 {
+            if i == n * 2 - 1 {
+                println!("{}", a[i]);
+            } else {
+                print!("{} ", a[i]);
+            }
+        }
+    }
+}
---
+++ 1526/src/bin/b.rs
@@ -0,0 +1,42 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+fn main() {
+    let mut sc = io_pro::Scanner::new(std::io::stdin().lock());
+    input!(sc = sc, t: usize);
+    for _ in 0..t {
+        input!(sc = sc, s: i64);
+        let v_1 = vec![
+            0, 0, 11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111, 1111111111,
+        ];
+
+        let mut buff = s;
+        while buff > 10 {
+            let mut index = 0;
+            let mut max = 0;
+            for (i, v) in buff
+                .to_string()
+                .chars()
+                .map(|c| (c as i64 - 48))
+                .enumerate()
+                .take(buff.to_string().len() - 1)
+            {
+                if max < v {
+                    max = v;
+                    index = i;
+                }
+            }
+
+            buff -= v_1[buff.to_string().len() - index];
+        }
+        println!("{}", if buff == 0 { "YES" } else { "NO" });
+    }
+}
