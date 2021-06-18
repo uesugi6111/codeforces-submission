bb55117e12fda4e3509d9411dd0766ceb32f05ba
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-06-18 16:33:45 +0000

    1537

---
+++ 1537/src/bin/d.rs
@@ -0,0 +1,47 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+fn main() {
+    let mut sc = io_pro::Scanner::new(std::io::stdin().lock());
+    input!(sc = sc, t: usize);
+    for _ in 0..t {
+        input!(sc = sc, n: usize);
+
+        let a = enum_divisors(n as i64);
+        if a.len() <= 2 {
+            println!("Bob");
+            continue;
+        }
+        let b = n as i64 / a[1];
+        println!("{}", if b % 2 == 1 { "Alice" } else { "Bob" });
+    }
+}
+
+pub fn enum_divisors(n: i64) -> Vec<i64> {
+    let mut res = vec![];
+    for i in 1..=(n as f64).sqrt() as i64 {
+        if n % i != 0 {
+            continue;
+        }
+        res.push(i);
+        if i.pow(2) != n {
+            res.push(n / i);
+        }
+    }
+    res
+}
+
+#[test]
+fn test() {
+    let mut a = enum_divisors(46);
+    a.sort_unstable();
+    dbg!(&a);
+}
