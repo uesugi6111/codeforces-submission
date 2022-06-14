c42c2ecea4e5de0272a7cf1edc5aae49410c1536
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2022-06-14 17:37:24 +0000

    1692

---
+++ 1692/src/bin/d.rs
@@ -0,0 +1,43 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+fn main() {
+    let mut sc = io_pro::Scanner::new(std::io::stdin().lock());
+    input!(sc = sc, t: usize);
+    for _ in 0..t {
+        input!(sc = sc, s: String, x: i64);
+        let ss = (
+            s.chars()
+                .take(2)
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap(),
+            s.chars()
+                .skip(3)
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap(),
+        );
+
+        let mut count = 0;
+        let mut buff = ss;
+        let mut is_ok = true;
+        while buff != ss || is_ok {
+            is_ok = false;
+
+            buff = ((buff.0 + (x + buff.1) / 60) % 24, (buff.1 + x) % 60);
+            if buff.0 == buff.1 / 10 + (buff.1 % 10) * 10 {
+                count += 1;
+            }
+        }
+        println!("{}", count);
+    }
+}
